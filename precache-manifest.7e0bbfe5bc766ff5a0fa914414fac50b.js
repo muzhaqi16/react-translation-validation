ffd19ac79d4045f88b051a7dc7d9cc3a96a9e2e2
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2020-10-16 16:35:55 +0200

    Updates

---
+++ precache-manifest.7e0bbfe5bc766ff5a0fa914414fac50b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d421116976f6a4a38572d749081211",
+    "url": "/muzhaqi16/react-translation-validation/index.html"
+  },
+  {
+    "revision": "777b823a6a56d6fbb641",
+    "url": "/muzhaqi16/react-translation-validation/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "13214feabc4548429acc",
+    "url": "/muzhaqi16/react-translation-validation/static/css/main.9ea96503.chunk.css"
+  },
+  {
+    "revision": "777b823a6a56d6fbb641",
+    "url": "/muzhaqi16/react-translation-validation/static/js/2.8de1d6f7.chunk.js"
+  },
+  {
+    "revision": "65f5ddb972ec20d2f34381b91122a87f",
+    "url": "/muzhaqi16/react-translation-validation/static/js/2.8de1d6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13214feabc4548429acc",
+    "url": "/muzhaqi16/react-translation-validation/static/js/main.cfefb9b5.chunk.js"
+  },
+  {
+    "revision": "486d22fa6ca74460a8cf",
+    "url": "/muzhaqi16/react-translation-validation/static/js/runtime-main.f76468b9.js"
+  }
+]);
